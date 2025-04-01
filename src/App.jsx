399cdd71cf0d4c7ceaed46971a17e1aa1968b612
mohit <singm2698@gmail.com>
2025-04-01 16:39:19 +0530

    create  page and  routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Singup from "./pages/Singup";
+import Singin from "./pages/Singin"
+import About from "./pages/About";
+import Profile from './pages/Profile';
+import Home from './pages/Home';
 
-export default function App( ){
-  return <h1 className='text-red-500'>App</h1>
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sing-in" element={<Singin />} />
+        <Route path="/sing-up" element={<Singup />} />
+        <Route path="/profile" element={<Profile />} />
+       
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      About is a simple component that displays information about your application and provides information about your application and its dependencies and dependencies as well as provides information about all of your application's dependencies       
+    </div>
+  )
+}
